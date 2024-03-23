b8be3bdd2062dfd49d08f569b91187d9769ba52e
ANIL MUNAGALA <164542969+AnilMunagala35@users.noreply.github.com>
2024-03-23 18:51:42 +0530

    Create day-2:  Search in Rotated Sorted Array.java

---
+++ day-2:  Search in Rotated Sorted Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static int search(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[s]) {
+                if (nums[mid] > target && nums[s] <= target) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[e]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+
+}
