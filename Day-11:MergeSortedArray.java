896c578b02c238cd122c9e1e82253f375d6c8329
ANIL MUNAGALA <164542969+AnilMunagala35@users.noreply.github.com>
2024-04-01 23:18:38 +0530

    Create Day-11:MergeSortedArray.java

---
+++ Day-11:MergeSortedArray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int[] arr = new int[m + n];
+        int k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                arr[k] = nums1[i];
+                i++;
+            } else {
+                arr[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < m) {
+            arr[k] = nums1[i];
+            k++;
+            i++;
+        }
+        while (j < n) {
+            arr[k] = nums2[j];
+            k++;
+            j++;
+        }
+        for (int l = 0; l < arr.length; l++) {
+            nums1[l] = arr[l];
+        }
+    }
+}
