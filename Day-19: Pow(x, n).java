0ab66b2d79c2e5c911d7fd4f5ebc258e2c1d9693
ANIL MUNAGALA <164542969+AnilMunagala35@users.noreply.github.com>
2024-04-09 17:05:14 +0530

    Update Day-19: Pow(x, n).java

--- Day-19: Pow(x, n).java
+++ Day-19: Pow(x, n).java
@@ -1,5 +1,17 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        if(n==0){
+            return 1;
+        }
+        double half = myPow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            if(n>0){
+                return half*half*x;
+            }else{
+                return half*(half/x);
+            }
+        }
     }
 }
